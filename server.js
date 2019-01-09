13682997dc9a0af9461aae8a311ac9d209e56c91
gollygeemister <46507390+gollygeemister@users.noreply.github.com>
2019-01-08 19:21:21 -0600

    add message receive functionality

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const token = 'NTMyMzYzMTAwMDk2MTY3OTM3.DxbZcQ.-5nE-1-GoNn8vOldTKZgVR_dVQ0';
 const client = new Discord.Client();
 
 
-client.on('ready', (msg)=> {
+client.on('ready', ()=> {
+    console.log(`Logged in as ${client.user.tag}!`);
     client.channels.find(x => x.name === 'test').send('Hello, I\'m connected!');
+});
+
+client.on('message', (msg) => {
+
+    if (msg.content === "!test"){
+        msg.channel.send(`Hello, ${msg.author}!`)
+    }
+
 });
 
 client.login(token);
