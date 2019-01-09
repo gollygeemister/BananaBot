a6c2596b04fb0bcafe838c00cdc2f23d58d9d1a6
gollygeemister <46507390+gollygeemister@users.noreply.github.com>
2019-01-08 19:03:36 -0600

    Set initial bot message

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-yeah
+const Discord = require('discord.js');
+const token = 'NTMyMzYzMTAwMDk2MTY3OTM3.DxbZcQ.-5nE-1-GoNn8vOldTKZgVR_dVQ0';
+
+const client = new Discord.Client();
+
+
+client.on('ready', (msg)=> {
+    client.channels.find(x => x.name === 'test').send('Hello, I\'m connected!');
+});
+
+client.login(token);
